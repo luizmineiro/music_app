65a4f4d541ff6d27d5c7f311d6f8fed47de24495
João Luiz <jluizmineiro@gmail.com>
2023-05-09 16:02:10 -0300

    aula 2

---
+++ lib/core/app/music_app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MusicAppColors {
+  MusicAppColors._();
+
+  //Black
+  static Color get primaryColor => const Color(0xFF000000);
+
+  //White
+  static Color get secondaryColor => const Color(0xFFFFFFFF);
+
+  //Green
+  static Color get tertiaryColor => const Color(0xFF779933);
+
+  //Red
+  static Color get errorColor => const Color(0xFFdc3545);
+}
